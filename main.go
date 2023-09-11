1ee368ed404cc0e342adacc332d906f638867156
Susmita Sanyal <susmita@viam.com>
2023-09-11 17:43:02 -0400

    adding main

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"xsensmtilib/xsense"
+
+	"github.com/edaniels/golog"
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/module"
+	"go.viam.com/utils"
+)
+
+func main() {
+	utils.ContextualMain(mainWithArgs, golog.NewDevelopmentLogger("mti-xsense"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error {
+	imu, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	imu.AddModelFromRegistry(ctx, sensor.API, xsense.Model)
+
+	err = imu.Start(ctx)
+	defer imu.Close(ctx)
+	if err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+	return nil
+}
